7a31e3e2630a1b6b66b2e81c8e4df41505c1100f
akash12patel12 <akash12patel12@gmail.com>
2023-01-25 08:44:28 +0530

    bcrypt successfully implemented

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -1,12 +1,50 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-exports.register =(req ,res)=>{
-    User.create({name: req.body.name, email : req.body.email, password : req.body.password, phone : req.body.phone }).then(r=>{
-        res.json(r);
-    }).catch(err=>{
-        console.log(err.errors[0].message);
+exports.register = (req, res) => {
+  bcrypt.hash(req.body.password, 3, function (err, hash) {
+    // Store hash in your password DB.
+    if (!err) {
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash,
+        phone: req.body.phone,
+      })
+        .then((r) => {
+          res.json(r);
+        })
+        .catch((err) => {
+          // console.log(err.errors[0].message);
 
-        res.json({errorMsg : err.errors[0].message})
-    })
-    // console.log(req.body);
-}
+          res.json({ errorMsg: err.errors[0].message });
+        });
+    } else {
+      res.json(err);
+    }
+  });
+};
+
+exports.login = (req, res) => {
+  
+
+
+
+
+
+
+  User.findOne({ where: { email: req.body.email } }).then((u) => {
+    if (u) {
+     bcrypt.compare(req.body.password, u.password ,  (err, result)=>{
+         if(result){
+          res.status(200).json({message : "Login success"})
+         }
+         else{
+          res.status(400).json({ err : err, message : "Login Failed,  Password Incorrect"})
+         }
+     })
+    } else {
+      res.status(404).json({ message: "user not found" });
+    }
+  });
+};
--- fe/index.js
+++ fe/index.js
@@ -15,3 +15,33 @@
        }
     })
 }
+
+
+function loginuser(e){
+    e.preventDefault();
+    const user  = {
+        email : e.target.email.value,
+        password : e.target.password.value
+    }
+    axios.post("http://localhost:3000/login",user).then(res=>{
+       alert(res.data.message);
+    }).catch(err=>{
+      alert(err.response.data.message);
+    });
+}
+
+
+
+
+
+////  Functions to switch from login to signup and backwards
+function enablesignup(){
+    document.getElementById("signupform").hidden = false;
+    document.getElementById("loginform").hidden = true;
+}
+function enablelogin(){
+    document.getElementById("loginform").hidden = false;
+    document.getElementById("signupform").hidden = true;
+}
+
+
